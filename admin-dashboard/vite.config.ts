0611cf0b5aba51ffc5e850f423c995a944faad8f
KOkoj <matej.kucera03@seznam.cz>
2025-07-17 13:59:16 +0200

    Add .env with production API URL

--- admin-dashboard/vite.config.ts
+++ admin-dashboard/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -21,4 +22,4 @@ export default defineConfig({
   define: {
     'process.env': process.env
   }
-}) 
+});
